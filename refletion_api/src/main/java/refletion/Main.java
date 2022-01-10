5625bcaec6ba613294b99fe055521e47cbbdb716
Claudeilton Dantas <fcd007@hotmail.com>
2022-01-10 20:08:22 -0300

    feat: add chamada de metodos refletion API

--- refletion_api/src/main/java/refletion/Main.java
+++ refletion_api/src/main/java/refletion/Main.java
@@ -15,10 +15,24 @@ public class Main {
 			System.out.println(field.getName() + " => " + field.getType());
 		}
 		
+		System.out.println();
+		
 		Method[] methods = tvClass.getMethods();
 		
 		for (Method method : methods) {
-			System.out.println(method.getName() + ", " + method.getReturnType() + Arrays.toString(method.getParameters()));
+			System.out.println(method.getName() + ", " + method.getReturnType() + ", " + Arrays.toString(method.getParameters()));
 		}
+		
+		System.out.println();
+		//chamada de método através da refletion API
+		Televisao tv1 = new Televisao();
+		int canal = 25;
+		Method  mudarCanal = tvClass.getDeclaredMethod("mudarCanal", int.class);
+		mudarCanal.invoke(tv1, canal);
+		
+		// fazer outra chamada da API
+		Televisao tv2 = new Televisao();
+		Method ligar = tvClass.getMethod("ligar");
+		ligar.invoke(tv2);
 	}
 }
