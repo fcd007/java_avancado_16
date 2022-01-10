706c6df96db3e8ffd79ff8deecb237a2e1a45619
Claudeilton Dantas <fcd007@hotmail.com>
2022-01-10 20:48:59 -0300

    feat: Annotation exemplo prático

---
+++ Annotation/src/main/java/annotation/reflection/Main.java
@@ -0,0 +1,27 @@
+package annotation.reflection;
+
+import java.lang.reflect.Method;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		MinhaClasse mc = new MinhaClasse();
+		
+		executar(mc);
+	}
+	
+	private static void executar(MinhaClasse mc) throws Exception{
+		@SuppressWarnings("unchecked")
+		Class<MinhaClasse> c = (Class<MinhaClasse>) mc.getClass();
+		
+		Method[] methods = c.getDeclaredMethods();
+		
+		for (Method method : methods) {
+			Executar annotation= method.getDeclaredAnnotation(Executar.class);
+			
+			if(annotation != null) {
+				method.invoke(mc);
+			}
+		}
+	}
+}
---
+++ Annotation/src/main/java/annotation/reflection/MinhaClasse.java
@@ -0,0 +1,18 @@
+package annotation.reflection;
+
+public class MinhaClasse {
+
+	
+	@Executar	
+	public void m1() {
+		System.out.println("m1()");
+	}
+	
+	public void m2() {
+		System.out.println("m2()");
+	}
+	
+	public void m3() {
+		System.out.println("m3()");
+	}
+}
