f9bcccabd9d7e1d06f6b8c9f21d1178e16dff5d0
Claudeilton Dantas <fcd007@hotmail.com>
2022-02-09 22:14:52 -0300

    feat: add inner class static

---
+++ innerClassesStatic/src/main/java/br/com/javacode/ClasseDeFora.java
@@ -0,0 +1,14 @@
+package br.com.javacode;
+
+public class ClasseDeFora {
+	
+	
+	static class ClassDeDentro {
+		private int valor;
+
+		public void imprimir() {
+			System.out.println(valor);
+		}
+	}
+
+}
---
+++ innerClassesStatic/src/main/java/br/com/javacode/Main.java
@@ -0,0 +1,12 @@
+package br.com.javacode;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ClasseDeFora.ClassDeDentro c = new ClasseDeFora.ClassDeDentro();
+		
+		c.imprimir();
+	}
+
+}
